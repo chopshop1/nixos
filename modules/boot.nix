087ff0f49d96e959206280ab6c24a7f11c4bd0bb
Your Name <27amad27@gmail.com>
2025-09-25 17:01:37 -0400

    Simplify boot configuration for compatibility

--- modules/boot.nix
+++ modules/boot.nix
@@ -5,23 +5,10 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.kernelModules = [ "amdgpu" ];
-  
+
   boot.kernelParams = [
     "quiet"
-    "splash"
-    "vga=current"
-    "rd.systemd.show_status=false"
-    "rd.udev.log_level=3"
-    "udev.log_priority=3"
   ];
-
-  boot.consoleLogLevel = 0;
-  boot.initrd.verbose = false;
-
-  boot.plymouth = {
-    enable = true;
-    theme = "breeze";
-  };
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
