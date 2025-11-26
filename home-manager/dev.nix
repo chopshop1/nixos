d7b66366107f20c1d1a9bc2d25aa0ebc69346afe
chopshop1 <27amad27@gmail.com>
2025-11-26 14:17:38 -0500

    update

--- home-manager/dev.nix
+++ home-manager/dev.nix
@@ -49,12 +49,20 @@
 
     # Development
     vscode
+
+    # Playwright and browsers
+    playwright-driver
+    chromium
+    # Firefox already included above
   ];
 
   # Environment variables
   home.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     SHELL = "${pkgs.zsh}/bin/zsh";
+    # Playwright configuration for NixOS
+    PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+    PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "1";
   };
 
   # Zsh configuration
